2bcdc52b886f0278c9f7af33231f11d5f19dfd8e
Will Sanders <williamsanders81@gmail.com>
2021-07-28 19:39:42 -0700

    uploading second version of exercise after watching lectures

--- Asynchronous_Code_in_JS/Async_and_Await/DeckOfCards.js
+++ Asynchronous_Code_in_JS/Async_and_Await/DeckOfCards.js
@@ -26,21 +26,46 @@
 
 //3. Build an HTML page that lets you draw cards from a deck. When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck.
 
-$('document').ready(async function(){
-    let deck = await axios.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    deckId = deck.data.deck_id
-})
+// $('document').ready(async function(){
+//     let deck = await axios.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+//     deckId = deck.data.deck_id
+// })
 
 const $deckContainer = $("#card-container")
-let deckId; 
-$("button").click(async ()=> {
-    console.log(deckId)
-    let cardRes = await axios.get(`http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-    let cardImage = cardRes.data.cards[0].image
-    console.log(cardRes.data.remaining)
-    $deckContainer.append($(`<img src=${cardImage}>`))
-    if (cardRes.data.remaining === 0) {
-        $("button").remove()
-        alert("That's all of them!")
+// let deckId; 
+// $("button").click(async ()=> {
+//     console.log(deckId)
+//     let cardRes = await axios.get(`http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+//     let cardImage = cardRes.data.cards[0].image
+//     console.log(cardRes.data.remaining)
+//     $deckContainer.append($(`<img src=${cardImage}>`))
+//     if (cardRes.data.remaining === 0) {
+//         $("button").remove()
+//         alert("That's all of them!")
+//     }
+// })
+
+
+deckCount = 52
+const deck = {
+    async init () {
+        let res = await axios.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        this.deckId = res.data.deck_id; 
+    }, 
+    async drawCard() {
+        let res = await axios.get(`http://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`)
+        
+        $deckContainer.append($(`<img src=${res.data.cards[0].image}>`))
+        deckCount --
+        if (deckCount === 0) {
+            $("button").remove()
+            alert("That's all of them!")
+        }
+        
     }
-})
+}
+deck.init()
+$("button").click(async ()=> {
+        deck.drawCard()
+       
+    })
